131ef54421d08cc261d78320f0d09ec648679231
Prashanth Jayaram Dev <prashanthj.076@gmail.com>
2018-03-18 17:26:44 +0530

    Springcloud-poc
    initialcommit :  spring cloud + angular + Eureka + Micro Service

---
+++ src/main/java/com/springCloudpoc/App.java
@@ -0,0 +1,13 @@
+package com.springCloudpoc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
